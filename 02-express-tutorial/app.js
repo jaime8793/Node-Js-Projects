44a8d33154573ce20390df1106824af48c50b447
James Owiti <jaime8793>
2024-04-23 13:07:04 +0300

    Exprees Day 2.1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,46 @@
-const http = require('http');
+const http = require("http");
+const { readFileSync } = require("fs");
+
+//get all files
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    const url = req.url
-    if (url === "/") {
-       // console.log('user hit the server');
-        res.writeHead(200, { 'content-type': 'text/html' });
-        res.write('<h1>home page</h1>');
-        res.end();
-    }
-      else if (url === "/about") {
-        //console.log("user hit the server");
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>About page</h1>");
-        res.end();
-    }
-      else  {
-        //console.log("user hit the server");
-        res.writeHead(404, { "content-type": "text/html" });
-        res.write("<h1>Cant Be found</h1>");
-        res.end();
-      }
+  console.log(req.url);
+  const url = req.url;
+  if (url === "/") {
+    // console.log('user hit the server');
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/styles.css") {
+    // console.log('user hit the server');
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  } else if (url === "/logo.svg") {
+    // console.log('user hit the server');
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    // console.log('user hit the server');
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  } else if (url === "/about") {
+    //console.log("user hit the server");
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About page</h1>");
+    res.end();
+  } else {
+    //console.log("user hit the server");
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Cant Be found</h1>");
+    res.end();
+  }
 });
 
-server.listen(5000)
+server.listen(5000);
