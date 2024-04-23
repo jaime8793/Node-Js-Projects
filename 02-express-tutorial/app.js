532203e6b12490c38a6354978b8dc5b9f5a253f7
James Owiti <jaime8793>
2024-04-23 12:38:14 +0300

    Exprees Day 2

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    const url = req.url
+    if (url === "/") {
+       // console.log('user hit the server');
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>home page</h1>');
+        res.end();
+    }
+      else if (url === "/about") {
+        //console.log("user hit the server");
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("<h1>About page</h1>");
+        res.end();
+    }
+      else  {
+        //console.log("user hit the server");
+        res.writeHead(404, { "content-type": "text/html" });
+        res.write("<h1>Cant Be found</h1>");
+        res.end();
+      }
+});
+
+server.listen(5000)
