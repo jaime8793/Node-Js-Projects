5e8ec94f79b69e3390504ef153547bfea616ab9c
James Owiti <jaime8793>
2024-04-23 15:06:00 +0300

    Exprees Day 2.2

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,46 +1,27 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-//get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  const url = req.url;
-  if (url === "/") {
-    // console.log('user hit the server');
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/styles.css") {
-    // console.log('user hit the server');
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  } else if (url === "/logo.svg") {
-    // console.log('user hit the server');
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    // console.log('user hit the server');
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  } else if (url === "/about") {
-    //console.log("user hit the server");
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About page</h1>");
-    res.end();
-  } else {
-    //console.log("user hit the server");
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Cant Be found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.send("Home page");
 });
 
-server.listen(5000);
+app.get("/", (req, res) => {
+  res.send("about page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1> resource not found </h1>");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
