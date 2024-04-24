33ae4b497371933e1e85883e8a6918e15496ac13
James Owiti <jaime8793>
2024-04-24 10:14:36 +0300

    Express Day 3.1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
-const {products} = require("./data.js")
+const { products } = require("./data.js");
 app.get("/", (req, res) => {
-  res.json(products);
+  //res.json(products);
+  res.send('<h2>Home page </h2><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  console.log(req.params);
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+    //return {product}
+  );
+  if (!singleProduct) {
+    res.status(404).send('Product does not exist')
+  }
+  res.json(singleProduct);
+});
+
+app.all("*", (req, res) => {
+  res.send("sorry we cant get that for you right now");
 });
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+  console.log("server is listening on port 5000...");
 });
