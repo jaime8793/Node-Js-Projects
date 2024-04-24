eb1238e0f32926dcc6df1b081faf99e94bd5c6a8
James Owiti <jaime8793>
2024-04-24 09:16:58 +0300

    Express Day 3

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-
-app.use(express.static("./public"));
-
+const {products} = require("./data.js")
 app.get("/", (req, res) => {
-  console.log("user hit the resource");
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-  // res.send("Home page");
-});
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1> resource not found </h1>");
+  res.json(products);
 });
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+  console.log('server is listening on port 5000...')
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
