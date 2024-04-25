47db5fff09c8b92d535a96a797b10a836f321032
James Owiti <jaime8793>
2024-04-25 10:19:24 +0300

    Express Day 4.3

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+let { people } = require("./data");
 
-//app.use([logger,authorize]);
+app.use(express.static("./methods-public"));
 
-//app.use(express.static('./public'))
-
-app.get("/", (req, res) => {
-  res.send("home");
-});
-
-app.get("/", (req, res) => {
-  res.send("About");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("products");
-});
-
-app.get("/api/items", (req, res) => {
-  res.send("items");
+app.get("/api/people", (req, res) => {
+  const individualPeople = people.map((person) => {
+    const { id, name } = person;
+    return { name, id };
+  });
+  res.status(200).json({ data: individualPeople });
 });
 
 app.listen(5000, () => {
