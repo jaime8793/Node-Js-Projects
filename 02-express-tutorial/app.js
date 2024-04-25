793f2b40f77b5c8ca187a54993e528404740dd78
James Owiti <jaime8793>
2024-04-25 09:10:57 +0300

    Express Day 4.1

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const logger = require('./logger')
 
+app.get("/",logger , (req, res) => {
+  res.send("home");
+});
 
+app.get("/", (req, res) => {
+  res.send("About");
+});
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
